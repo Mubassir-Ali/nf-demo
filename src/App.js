58470e45b43c850b60a9efae020eaa67f6a8cfa4
Mubassir-Ali <mubassir11@gmail.com>
2021-01-06 20:29:44 +0500

    nf-demo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+        <div className="card">
+          <img className="card_img" src="https://occ-0-3736-64.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABeu5Oz4CGncoVEvCDhLelkyQc7H0KsaLyauqCprkOTTePn46kiFYRe1AUg5UkCp9ka7RTgyc8mKrSy7w9VFeQDokfmzy.jpg?r=077" alt="myImage"></img>
+          
+          <div className="card_info">
+            <a href="https://www.netflix.com/pk/title/80100172"><p>Dark</p></a>
+
+
+          </div>
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
